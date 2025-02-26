5155026a4626e1e6d06bf1eba138a15d207c58a7
Praxco <151080810+Praxco@users.noreply.github.com>
2025-02-26 16:56:56 -0600

    Create cgs_4-bit_RCA_behavioral.sv

---
+++ cgs_4-bit_RCA_behavioral.sv
@@ -0,0 +1,14 @@
+// 4-Bit Ripple Carry Adder
+// Written by Cole Garrett Shoemaker
+
+
+
+
+module four_bit_RCA (A, B, Cin, S, Cout);
+  input [3:0] A, B;
+  input Cin;
+  output [3:0] S;
+  output Cout;
+
+  
+  
