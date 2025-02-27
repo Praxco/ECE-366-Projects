0290b567dedac7c1c50330e5d0414b6541973a34
Praxco <151080810+Praxco@users.noreply.github.com>
2025-02-26 20:59:30 -0600

    Update problem1_final.sv

--- Project 1/Problem 1/problem1_final.sv
+++ Project 1/Problem 1/problem1_final.sv
@@ -1,1 +1,10 @@
+module one_bit_full_adder(A, B, Cin, S, Cout);
+  input A, B, Cin;
+  output S, Cout;
+
+  assign S = A ^ B ^ Cin;
+  assign Cout = (A & B) | (A ^ B & Cin);
+  // could also write: assign{S, Cout} = A + B + Cin;
+endmodule
+
 
