9112c3d941e67fdace673ad7177ee925a2586279
Praxco <151080810+Praxco@users.noreply.github.com>
2025-02-26 19:29:15 -0600

    Update cgs_one_bit_FA_behavioral.sv

--- cgs_one_bit_FA_behavioral.sv
+++ cgs_one_bit_FA_behavioral.sv
@@ -6,4 +6,5 @@ module one_bit_full_adder(A, B, Cin, S, Cout);
 
   assign S = A ^ B ^ Cin;
   assign Cout = (A & B) | (A ^ B & Cin);
+  // could also write: assign{S, Cout} = A + B + Cin;
 endmodule
