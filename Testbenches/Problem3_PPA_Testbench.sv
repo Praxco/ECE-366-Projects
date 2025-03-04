ef37181f3ef8313ac08fac583d81f349a048ac3e
cgp2201 <carlosperez2201g@gmail.com>
2025-03-04 11:55:43 -0600

    Update Problem3_PPA_Testbench.sv

--- Testbenches/Problem3_PPA_Testbench.sv
+++ Testbenches/Problem3_PPA_Testbench.sv
@@ -1,50 +1,24 @@
 module TestBench();
-
+  
   reg cin, p_0;
   reg [15:0] A_Value, B_Value;
-
   wire [15:0] Sum;
 
   Main DUT0(Sum, A_Value, B_Value, cin, p_0);
 
-  initial begin
-    A_Value = 16'b0000000000000000; 
-    B_Value = 16'b0000000000000000; 
-    cin = 0; p_0 = 0;
-    #10
-    $display("Sum = %b", Sum);
-    
-	A_Value = 16'b0000000000001111; 
-    B_Value = 16'b0000000000000001; 
-    cin = 0; p_0 = 0;
-    #10
-    $display("Sum = %b", Sum);
-
-    A_Value = 16'b0000000001110000; 
-    B_Value = 16'b0000000000001111; 
-    cin = 0; p_0 = 0;
-    #10
-    $display("Sum = %b", Sum);
-    
-    A_Value = 16'b1001110000000000; 
-    B_Value = 16'b0001110010000000; 
-    cin = 1; p_0 = 0;
-    #10
-    $display("Sum = %b", Sum);
-    
-    A_Value = 16'b0001000001110000; 
-    B_Value = 16'b1100110000000110; 
-    cin = 1; p_0 = 0;
-    #10
-    $display("Sum = %b", Sum);
-    
-    A_Value = 16'b0000100000011100; 
-    B_Value = 16'b1101111000011100; 
-    cin = 0; p_0 = 0;
-    #10
-    $display("Sum = %b", Sum);
-    
-    $finish;
+    initial begin
+      $display("Sum	A    B	Cin p_0");
+      $display("------------------------");
+      $monitor("%h, %h, %h, %b, %b", Sum, A_Value, B_Value, cin, p_0);
+    {A_Value, B_Value} = 16'h0000; {cin, p_0} = 0;
+    #5 A_Value = 16'h0010; B_Value = 16'h0020;
+    #5 A_Value = 16'h0110; B_Value = 16'h0110; cin = 1;
+    #5 A_Value = 16'h1010; B_Value = 16'h4000;
+    #5 A_Value = 16'h0043; B_Value = 16'h5021; cin = 0;
+    #5 A_Value = 16'h0723; B_Value = 16'h0429;
+    #5 A_Value = 16'h0553; B_Value = 16'h6420;
+  
+   #50 $finish;
 
   end
 endmodule
