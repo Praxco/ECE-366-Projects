c04a824e6c8700f1a2e3d4a04b4ca5cfbf846d4e
cgp2201 <carlosperez2201g@gmail.com>
2025-03-10 20:19:03 -0500

    Update 16-bit_parallel_prefix_adder.sv

--- Project 1/Problem 3/16-bit_parallel_prefix_adder.sv
+++ Project 1/Problem 3/16-bit_parallel_prefix_adder.sv
@@ -24,99 +24,82 @@ module P_G_i(G, P, A, B);					// ith bit P and G
 endmodule
 
 
-module Main(Sum, A_Value, B_Value, cin, p_0);
+module Main(Sum, A, B, Cin, p_0);
   output [15:0] Sum;
-  input cin, p_0;
-  input [15:0] A_Value, B_Value;
-  
-  wire c0_out_0 , c0_out_1 , c2_out_0 , c2_out_1 , c6_out_0 , c6_out_1 , c8_out_0 , c8_out_1 , c10_out_0 , c10_out_1 , c11_out_0 , c11_out_1 , c13_out_0,
-  c13_out_1 , c14_out_0 , c14_out_1 ,  c9_out_0 , c9_out_1 , c7_out_0 , c7_out_1 , c5_out_0 , c5_out_1 , c4_out_0 , c4_out_1 , c3_out_0 , c3_out_1,
-  c12_out_0, c12_out_1 , 
-  
-  Sum_62_out, Sum_61_out,  Sum_51_out, Sum_50_out, Sum_52_out, Sum_53_out, Sum_56_out, Sum_55_out,  Sum_49_out, Sum_57_out,
-  Sum_59_out, Sum_63_out, Sum_58_out, Sum_60_out, Sum_0_out, Sum_54_out, 
-  P_G_i_20_out_0, P_G_i_20_out_1, P_G_i_19_out_0, P_G_i_19_out_1, P_G_i_18_out_0, P_G_i_18_out_1, P_G_i_17_out_0, P_G_i_17_out_1, P_G_i_15_out_0,
-  P_G_i_15_out_1, P_G_i_14_out_0, P_G_i_14_out_1, P_G_i_13_out_0, P_G_i_13_out_1, P_G_i_12_out_0, P_G_i_12_out_1, P_G_i_9_out_0, P_G_i_9_out_1,
-  P_G_i_8_out_0, P_G_i_8_out_1, P_G_i_6_out_0, P_G_i_6_out_1, P_G_i_3_out_0, P_G_i_3_out_1, P_G_i_2_out_0, P_G_i_2_out_1, P_G_i_5_out_0, P_G_i_5_out_1,
-  P_G_i_4_out_0, P_G_i_4_out_1,
-  
-  temp14to13_out_0, temp14to13_out_1, temp14to11_out_0, temp14to11_out_1, temp14to7_out_0, temp14to7_out_1,  temp13to11_out_0, temp13to11_out_1,
-  temp13to7_out_0, temp13to7_out_1, temp12to11_out_0, temp12to11_out_1, temp12to7_out_0, temp11to7_out_1, temp10to9_out_0, temp10to9_out_1, temp10to7_out_0,
-  temp10to7_out_1,temp9to7_out_0, temp9to7_out_1, temp8to7_out_0, temp8to7_out_1, temp6to5_out_0, temp6to5_out_1, temp6to3_out_0, temp6to3_out_1,
-  temp4to3_out_0, temp4to3_out_1, temp2to1_out_0, temp2to1_out_1, temp5to3_out_0, temp5to3_out_1;
-  
-  assign Sum =
-{Sum_0_out,Sum_49_out,Sum_50_out,Sum_51_out,Sum_52_out,Sum_53_out,Sum_54_out,Sum_55_out,Sum_56_out,Sum_57_out,Sum_58_out,Sum_59_out,Sum_60_out,Sum_61_out,Sum_62_out,Sum_63_out};
-  
-  Sum Sum_63(Sum_63_out, A_Value[0], B_Value[0], cin);
-  Sum Sum_62(Sum_62_out, A_Value[1], B_Value[1], c0_out_1 );
-  Sum Sum_61(Sum_61_out, A_Value[2], B_Value[2], c1_out_1 );
-  Sum Sum_60(Sum_60_out, A_Value[3], B_Value[3], c2_out_1 ); 
-  Sum Sum_59(Sum_59_out, A_Value[4], B_Value[4], c3_out_1 );
-  Sum Sum_58(Sum_58_out, A_Value[5], B_Value[5], c4_out_1 );
-  Sum Sum_57(Sum_57_out, A_Value[6], B_Value[6], c5_out_1 ); 
-  Sum Sum_56(Sum_56_out, A_Value[7], B_Value[7], c6_out_1 );  
-  Sum Sum_55(Sum_55_out, A_Value[8], B_Value[8], c7_out_1 );
-  Sum Sum_54(Sum_54_out, A_Value[9], B_Value[9], c8_out_1 );
-  Sum Sum_53(Sum_53_out, A_Value[10], B_Value[10], c9_out_1 );
-  Sum Sum_52(Sum_52_out, A_Value[11], B_Value[11], c10_out_1 );
-  Sum Sum_51(Sum_51_out, A_Value[12], B_Value[12], c11_out_1 );
-  Sum Sum_50(Sum_50_out, A_Value[13], B_Value[13], c12_out_1 );
-  Sum Sum_49(Sum_49_out, A_Value[14], B_Value[14], c13_out_1 );
-  Sum Sum_0(Sum_0_out, A_Value[15], B_Value[15], c14_out_1 ); 
-  
+  input Cin, p_0;
+  input [15:0] A, B;
    
-  P_G_i P_G_i_20(P_G_i_20_out_0, P_G_i_20_out_1, A_Value[15], B_Value[15]);
-  P_G_i P_G_i_19(P_G_i_19_out_0, P_G_i_19_out_1, A_Value[14], B_Value[14]);    
-  P_G_i P_G_i_18(P_G_i_18_out_0, P_G_i_18_out_1, A_Value[13], B_Value[13]);  
-  P_G_i P_G_i_17(P_G_i_17_out_0, P_G_i_17_out_1, A_Value[12], B_Value[12]);    
-  P_G_i P_G_i_16(P_G_i_16_out_0, P_G_i_16_out_1, A_Value[11], B_Value[11]);  
-  P_G_i P_G_i_15(P_G_i_15_out_0, P_G_i_15_out_1, A_Value[10], B_Value[10]);  
-  P_G_i P_G_i_14(P_G_i_14_out_0, P_G_i_14_out_1, A_Value[9], B_Value[9]);  
-  P_G_i P_G_i_13(P_G_i_13_out_0, P_G_i_13_out_1, A_Value[8], B_Value[8]);  
-  P_G_i P_G_i_12(P_G_i_12_out_0, P_G_i_12_out_1, A_Value[7], B_Value[7]);
-  P_G_i P_G_i_9(P_G_i_9_out_0, P_G_i_9_out_1, A_Value[6], B_Value[6]);    
-  P_G_i P_G_i_8(P_G_i_8_out_0, P_G_i_8_out_1, A_Value[5], B_Value[5]);  
-  P_G_i P_G_i_6(P_G_i_6_out_0, P_G_i_6_out_1, A_Value[4], B_Value[4]);
-  P_G_i P_G_i_5(P_G_i_5_out_0, P_G_i_5_out_1, A_Value[3], B_Value[3]);
-  P_G_i P_G_i_4(P_G_i_4_out_0, P_G_i_4_out_1, A_Value[2], B_Value[2]);  
-  P_G_i P_G_i_3(P_G_i_3_out_0, P_G_i_3_out_1, A_Value[1], B_Value[1]);
-  P_G_i P_G_i_2(P_G_i_2_out_0, P_G_i_2_out_1, A_Value[0], B_Value[0]); 
+  wire [15:0] s, PN, GN, G, P; 
+  wire [16:0] tempPN, tempGN;
+   
+  P_G_i P_G_i_15(.G(G[15]), .P(P[15]), .A(A[15]), .B(B[15]));
+  P_G_i P_G_i_14(.G(G[14]), .P(P[14]), .A(A[14]), .B(B[14]));    
+  P_G_i P_G_i_13(.G(G[13]), .P(P[13]), .A(A[13]), .B(B[13]));  
+  P_G_i P_G_i_12(.G(G[12]), .P(P[12]), .A(A[12]), .B(B[12]));    
+  P_G_i P_G_i_11(.G(G[11]), .P(P[11]), .A(A[11]), .B(B[11]));  
+  P_G_i P_G_i_10(.G(G[10]), .P(P[10]), .A(A[10]), .B(B[10]));  
+  P_G_i P_G_i_9(.G(G[9]), .P(P[9]), .A(A[9]), .B(B[9]));  
+  P_G_i P_G_i_8(.G(G[8]), .P(P[8]), .A(A[8]), .B(B[8]));  
+  P_G_i P_G_i_7(.G(G[7]), .P(P[7]), .A(A[7]), .B(B[7]));
+  P_G_i P_G_i_6(.G(G[6]), .P(P[6]), .A(A[6]), .B(B[6]));    
+  P_G_i P_G_i_5(.G(G[5]), .P(P[5]), .A(A[5]), .B(B[5]));  
+  P_G_i P_G_i_4(.G(G[4]), .P(P[4]), .A(A[4]), .B(B[4]));
+  P_G_i P_G_i_3(.G(G[3]), .P(P[3]), .A(A[3]), .B(B[3]));
+  P_G_i P_G_i_2(.G(G[2]), .P(P[2]), .A(A[2]), .B(B[2]));  
+  P_G_i P_G_i_1(.G(G[1]), .P(P[1]), .A(A[1]), .B(B[1]));
+  P_G_i P_G_i_0(.G(G[0]), .P(P[0]), .A(A[0]), .B(B[0])); 
   
-  Intermediate_P_G c0 (c0_out_0 , c0_out_1 , p_0, P_G_i_2_out_1, cin, P_G_i_2_out_0);
-  Intermediate_P_G c1 (c1_out_0 , c1_out_1 , c0_out_0 , P_G_i_3_out_1, c0_out_1 , P_G_i_3_out_0);
-  Intermediate_P_G c2 (c2_out_0 , c2_out_1 , c0_out_0 , temp2to1_out_0 , c0_out_1 , temp2to1_out_1 );
-  Intermediate_P_G c3 (c3_out_0 , c3_out_1 , c2_out_0 , P_G_i_5_out_1, c2_out_1 , P_G_i_5_out_0);
-  Intermediate_P_G c4 (c4_out_0 , c4_out_1 , c2_out_0 , temp4to3_out_0 , c2_out_1 , temp4to3_out_1 );
-  Intermediate_P_G c5 (c5_out_0 , c5_out_1 , c2_out_0 , temp5to3_out_0 , c2_out_1 , temp5to3_out_1 );
-  Intermediate_P_G c6 (c6_out_0 , c6_out_1 , c2_out_0 , temp6to3_out_0 , c2_out_1 , temp6to3_out_1 );
-  Intermediate_P_G c7 (c7_out_0 , c7_out_1 , c6_out_0 , P_G_i_12_out_1, c6_out_1 , P_G_i_12_out_0);
-  Intermediate_P_G c8 (c8_out_0 , c8_out_1 , c6_out_0 , temp8to7_out_0 , c6_out_1 , temp8to7_out_1 );
-  Intermediate_P_G c9 (c9_out_0 , c9_out_1 , c6_out_0 , temp9to7_out_0 , c6_out_1 , temp9to7_out_1 );
-  Intermediate_P_G c10 (c10_out_0 , c10_out_1 , c6_out_0 , temp10to7_out_0 , c6_out_1 , temp10to7_out_1 );
-  Intermediate_P_G c11 (c11_out_0 , c11_out_1 , c6_out_0 , temp11to7_out_0 , c6_out_1 , temp11to7_out_1 );
-  Intermediate_P_G c12 (c12_out_0 , c12_out_1 , c6_out_0 , temp12to7_out_0 , c6_out_1 , temp12to7_out_1 );
-  Intermediate_P_G c13 (c13_out_0 , c13_out_1 , c6_out_0 , temp13to7_out_0 , c6_out_1 , temp13to7_out_1 );
-  Intermediate_P_G c14 (c14_out_0 , c14_out_1 , c6_out_0 , temp14to7_out_0 , c6_out_1 , temp14to7_out_1 );
+  Intermediate_P_G c0 (.PN(PN[0]) , .GN(GN[0]) , .PR(p_0), .PL(P[0]), .GR(Cin), .GL(G[0]));
+  Intermediate_P_G c1 (.PN(PN[1]) , .GN(GN[1]) , .PR(PN[0]) , .PL(P[1]), .GR(GN[0]) , .GL(G[1]));
+  Intermediate_P_G c2 (.PN(PN[2]) , .GN(GN[2]) , .PR(PN[0]) , .PL(tempPN[0]) , .GR(GN[0]) , .GL(tempGN[0]) );
+  Intermediate_P_G c3 (.PN(PN[3]) , .GN(GN[3]) , .PR(PN[2]) , .PL(P[3]), .GR(GN[2]) , .GL(G[3]));
+  Intermediate_P_G c4 (.PN(PN[4]) , .GN(GN[4]) , .PR(PN[2]) , .PL(tempPN[1]) , .GR(GN[2]) , .GL(tempGN[1]) );
+  Intermediate_P_G c5 (.PN(PN[5]) , .GN(GN[5]) , .PR(PN[2]) , .PL(tempPN[2]) , .GR(GN[2]) , .GL(tempGN[2]) );
+  Intermediate_P_G c6 (.PN(PN[6]) , .GN(GN[6]) , .PR(PN[2]) , .PL(tempPN[3]) , .GR(GN[2]) , .GL(tempGN[3]));
+  Intermediate_P_G c7 (.PN(PN[7]) , .GN(GN[7]) , .PR(PN[6]) , .PL(P[7]), .GR(GN[6]) , .GL(G[7]));
+  Intermediate_P_G c8 (.PN(PN[8]) , .GN(GN[8]) , .PR(PN[6]) , .PL(tempPN[5]) , .GR(GN[6]) , .GL(tempGN[5]) );
+  Intermediate_P_G c9 (.PN(PN[9]) , .GN(GN[9]) , .PR(PN[6]) , .PL(tempPN[6]) , .GR(GN[6]) , .GL(tempGN[6]) );
+  Intermediate_P_G c10 (.PN(PN[10]) , .GN(GN[10]) , .PR(PN[6]) , .PL(tempPN[7]) , .GR(GN[6]) , .GL(tempGN[7]) );
+  Intermediate_P_G c11 (.PN(PN[11]) , .GN(GN[11]) , .PR(PN[6]) , .PL(tempPN[9]) , .GR(GN[6]) , .GL(tempGN[9]) );
+  Intermediate_P_G c12 (.PN(PN[12]) , .GN(GN[12]) , .PR(PN[6]) , .PL(tempPN[10]) , .GR(GN[6]) , .GL(tempGN[10]) );
+  Intermediate_P_G c13 (.PN(PN[13]) , .GN(GN[13]) , .PR(PN[6]) , .PL(tempPN[12]) , .GR(GN[6]) , .GL(tempGN[12]) );
+  Intermediate_P_G c14 (.PN(PN[14]) , .GN(GN[14]) , .PR(PN[6]) , .PL(tempPN[14]) , .GR(GN[6]) , .GL(tempGN[14]) );
   
+  Intermediate_P_G temp14to13 (.PN(tempPN[16]) , .GN(tempGN[16]) , .PR(P[13]), .PL(P[14]), .GR(G[13]), .GL(G[14]));
+  Intermediate_P_G temp14to11 (.PN(tempPN[15]) , .GN(tempGN[15]) , .PR(tempGN[11]) , .PL(tempPN[16]) , .GR(tempPN[11]) , .GL(tempGN[16]) );
+  Intermediate_P_G temp14to7 (.PN(tempPN[14]) , .GN(tempGN[14]) , .PR(tempPN[7]) , .PL(tempPN[15]) , .GR(tempGN[7]) , .GL(tempGN[15]) );
+  Intermediate_P_G temp13to11 (.PN(tempPN[13]) , .GN(tempGN[13]) , .PR(tempPN[11]) , .PL(P[13]), .GR(tempGN[11]) , .GL(G[13]));
+  Intermediate_P_G temp13to7 (.PN(tempPN[12]) , .GN(tempGN[12]) , .PR(tempPN[7]) , .PL(tempPN[13]) , .GR(tempGN[7]) , .GL(tempGN[13]) );
+  Intermediate_P_G temp12to11 (.PN(tempPN[11]) , .GN(tempGN[11]) , .PR(P[11]), .PL(P[12]), .GR(G[11]), .GL(G[12]));
+  Intermediate_P_G temp12to7 (.PN(tempPN[10]) , .GN(tempGN[10]) , .PR(tempPN[7]) , .PL(tempPN[11]) , .GR(tempGN[7]) , .GL(tempGN[11]) );
+  Intermediate_P_G temp11to7 (.PN(tempPN[9]) , .GN(tempGN[9]) , .PR(tempPN[7]) , .PL(P[11]), .GR(tempGN[7]) , .GL(G[11]));
+  Intermediate_P_G temp10to9 (.PN(tempPN[8]) , .GN(tempGN[8]) , .PR(P[9]), .PL(P[10]), .GR(G[9]), .GL(G[10]));
+  Intermediate_P_G temp10to7 (.PN(tempPN[7]) , .GN(tempGN[7]) , .PR(tempPN[5]) , .PL(tempPN[8]) , .GR(tempGN[5]) , .GL(tempGN[8]) );
+  Intermediate_P_G temp9to7 (.PN(tempPN[6]) , .GN(tempGN[6]) , .PR(tempPN[5]) , .PL(P[9]), .GR(tempGN[5]) , .GL(G[9]));
+  Intermediate_P_G temp8to7 (.PN(tempPN[5]) , .GN(tempGN[5]) , .PR(P[7]), .PL(P[8]), .GR(G[7]), .GL(G[8]));
+  Intermediate_P_G temp6to5 (.PN(tempPN[4]) , .GN(tempGN[4]) , .PR(P[5]), .PL(P[6]), .GR(G[5]), .GL(G[6]));
+  Intermediate_P_G temp6to3 (.PN(tempPN[3]) , .GN(tempGN[3]) , .PR(tempPN[1]) , .PL(tempPN[4]) , .GR(tempGN[1]) , .GL(tempGN[4]) );
+  Intermediate_P_G temp5to3 (.PN(tempPN[2]) , .GN(tempGN[2]) , .PR(tempPN[1]) , .PL(P[5]), .GR(tempGN[1]) , .GL(G[5]));
+  Intermediate_P_G temp4to3 (.PN(tempPN[1]) , .GN(tempGN[1]) , .PR(P[3]), .PL(P[4]), .GR(G[3]), .GL(G[4]));
+  Intermediate_P_G temp2to1 (.PN(tempPN[0]) , .GN(tempGN[0]) , .PR(P[1]), .PL(P[2]), .GR(G[1]), .GL(G[2]));
   
-  Intermediate_P_G temp14to13 (temp14to13_out_0 , temp14to13_out_1 , P_G_i_18_out_1, P_G_i_19_out_1, P_G_i_18_out_0, P_G_i_19_out_0);
-  Intermediate_P_G temp14to11 (temp14to11_out_0 , temp14to11_out_1 , temp12to11_out_1 , temp14to13_out_0 , temp12to11_out_0 , temp14to13_out_1 );
-  Intermediate_P_G temp14to7 (temp14to7_out_0 , temp14to7_out_1 , temp10to7_out_0 , temp14to11_out_0 , temp10to7_out_1 , temp14to11_out_1 );
-  Intermediate_P_G temp13to11 (temp13to11_out_0 , temp13to11_out_1 , temp12to11_out_0 , P_G_i_18_out_1, temp12to11_out_1 , P_G_i_18_out_0);
-  Intermediate_P_G temp13to7 (temp13to7_out_0 , temp13to7_out_1 , temp10to7_out_0 , temp13to11_out_0 , temp10to7_out_1 , temp13to11_out_1 );
-  Intermediate_P_G temp12to11 (temp12to11_out_0 , temp12to11_out_1 , P_G_i_16_out_1, P_G_i_17_out_1, P_G_i_16_out_0, P_G_i_17_out_0);
-  Intermediate_P_G temp12to7 (temp12to7_out_0 , temp12to7_out_1 , temp10to7_out_0 , temp12to11_out_0 , temp10to7_out_1 , temp12to11_out_1 );
-  Intermediate_P_G temp11to7 (temp11to7_out_0 , temp11to7_out_1 , temp10to7_out_0 , P_G_i_16_out_1, temp10to7_out_1 , P_G_i_16_out_0);
-  Intermediate_P_G temp10to9 (temp10to9_out_0 , temp10to9_out_1 , P_G_i_14_out_1, P_G_i_15_out_1, P_G_i_14_out_0, P_G_i_15_out_0);
-  Intermediate_P_G temp10to7 (temp10to7_out_0 , temp10to7_out_1 , temp8to7_out_0 , temp10to9_out_0 , temp8to7_out_1 , temp10to9_out_1 );
-  Intermediate_P_G temp9to7 (temp9to7_out_0 , temp9to7_out_1 , temp8to7_out_0 , P_G_i_14_out_1, temp8to7_out_1 , P_G_i_14_out_0);
-  Intermediate_P_G temp8to7 (temp8to7_out_0 , temp8to7_out_1 , P_G_i_12_out_1, P_G_i_13_out_1, P_G_i_12_out_0, P_G_i_13_out_0);
-  Intermediate_P_G temp6to5 (temp6to5_out_0 , temp6to5_out_1 , P_G_i_8_out_1, P_G_i_9_out_1, P_G_i_8_out_0, P_G_i_9_out_0);
-  Intermediate_P_G temp6to3 (temp6to3_out_0 , temp6to3_out_1 , temp4to3_out_0 , temp6to5_out_0 , temp4to3_out_1 , temp6to5_out_1 );
-  Intermediate_P_G temp5to3 (temp5to3_out_0 , temp5to3_out_1 , temp4to3_out_0 , P_G_i_8_out_1, temp4to3_out_1 , P_G_i_8_out_0);
-  Intermediate_P_G temp4to3 (temp4to3_out_0 , temp4to3_out_1 , P_G_i_5_out_1, P_G_i_6_out_1, P_G_i_5_out_0, P_G_i_6_out_0);
-  Intermediate_P_G temp2to1 (temp2to1_out_0 , temp2to1_out_1 , P_G_i_3_out_1, P_G_i_4_out_1, P_G_i_3_out_0, P_G_i_4_out_0);
+  Sum Sum0(.S(s[0]), .A(A[0]), .B(B[0]), .Cin(Cin));
+  Sum Sum1(.S(s[1]), .A(A[1]), .B(B[1]), .Cin(GN[0]));
+  Sum Sum2(.S(s[2]), .A(A[2]), .B(B[2]), .Cin(GN[1]));
+  Sum Sum3(.S(s[3]), .A(A[3]), .B(B[3]), .Cin(GN[2])); 
+  Sum Sum4(.S(s[4]), .A(A[4]), .B(B[4]), .Cin(GN[3]));
+  Sum Sum5(.S(s[5]), .A(A[5]), .B(B[5]), .Cin(GN[4]));
+  Sum Sum6(.S(s[6]), .A(A[6]), .B(B[6]), .Cin(GN[5])); 
+  Sum Sum7(.S(s[7]), .A(A[7]), .B(B[7]), .Cin(GN[6]));  
+  Sum Sum8(.S(s[8]), .A(A[8]), .B(B[8]), .Cin(GN[7]));
+  Sum Sum9(.S(s[9]), .A(A[9]), .B(B[9]), .Cin(GN[8]));
+  Sum Sum10(.S(s[10]), .A(A[10]), .B(B[10]), .Cin(GN[9]));
+  Sum Sum11(.S(s[11]), .A(A[11]), .B(B[11]), .Cin(GN[10]));
+  Sum Sum12(.S(s[12]), .A(A[12]), .B(B[12]), .Cin(GN[11]));
+  Sum Sum13(.S(s[13]), .A(A[13]), .B(B[13]), .Cin(GN[12]));
+  Sum Sum14(.S(s[14]), .A(A[14]), .B(B[14]), .Cin(GN[13]));
+  Sum Sum15(.S(s[15]), .A(A[15]), .B(B[15]), .Cin(GN[14])); 
   
-  
+  assign Sum = s[15:0];
+ 
 endmodule
