e9116f32923b058dc55a2df064db0655895958b5
cgp2201 <carlosperez2201g@gmail.com>
2025-03-10 20:20:23 -0500

    Update 16-bit_parallel_prefix_adder.sv

--- Project 1/Problem 3/16-bit_parallel_prefix_adder.sv
+++ Project 1/Problem 3/16-bit_parallel_prefix_adder.sv
@@ -103,3 +103,29 @@ module Main(Sum, A, B, Cin, p_0);
   assign Sum = s[15:0];
  
 endmodule
+
+
+module TestBench();
+  
+  reg Cin, p_0;
+  reg [15:0] A, B;
+  wire [15:0] Sum;
+
+  Main DUT0(Sum, A, B, Cin, p_0);
+
+    initial begin
+      $display("  Sum\tA\tB\tCin\tp_0");
+      $display("------------------------------------");
+      $monitor("%h,\t %h,\t %h,\t %b,\t %b", Sum, A, B, Cin, p_0);
+      {A, B} = 16'h0000; {Cin, p_0} = 0;
+    #5 A = 16'h0010; B = 16'h0020;
+    #5 A = 16'h0110; B = 16'h0110; Cin = 1;
+    #5 A = 16'h1010; B = 16'h4000;
+    #5 A = 16'h0043; B = 16'h5021; Cin = 0;
+    #5 A = 16'h0723; B = 16'h0429;
+    #5 A = 16'h0553; B = 16'h6420;
+  
+   #50 $finish;
+
+  end
+endmodule
