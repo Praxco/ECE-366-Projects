b9b2f20b0fb653ee189855588ed4dbeb7a328c5b
cgp2201 <carlosperez2201g@gmail.com>
2025-03-10 20:18:06 -0500

    Update Problem3_PPA_Testbench.sv

--- Testbenches/Problem3_PPA_Testbench.sv
+++ Testbenches/Problem3_PPA_Testbench.sv
@@ -1,22 +1,22 @@
 module TestBench();
   
-  reg cin, p_0;
-  reg [15:0] A_Value, B_Value;
+  reg Cin, p_0;
+  reg [15:0] A, B;
   wire [15:0] Sum;
 
-  Main DUT0(Sum, A_Value, B_Value, cin, p_0);
+  Main DUT0(Sum, A, B, Cin, p_0);
 
     initial begin
       $display("  Sum\tA\tB\tCin\tp_0");
       $display("------------------------------------");
-      $monitor("%h,\t %h,\t %h,\t %b,\t %b", Sum, A_Value, B_Value, cin, p_0);
-    {A_Value, B_Value} = 16'h0000; {cin, p_0} = 0;
-    #5 A_Value = 16'h0010; B_Value = 16'h0020;
-    #5 A_Value = 16'h0110; B_Value = 16'h0110; cin = 1;
-    #5 A_Value = 16'h1010; B_Value = 16'h4000;
-    #5 A_Value = 16'h0043; B_Value = 16'h5021; cin = 0;
-    #5 A_Value = 16'h0723; B_Value = 16'h0429;
-    #5 A_Value = 16'h0553; B_Value = 16'h6420;
+      $monitor("%h,\t %h,\t %h,\t %b,\t %b", Sum, A, B, Cin, p_0);
+      {A, B} = 16'h0000; {Cin, p_0} = 0;
+    #5 A = 16'h0010; B = 16'h0020;
+    #5 A = 16'h0110; B = 16'h0110; Cin = 1;
+    #5 A = 16'h1010; B = 16'h4000;
+    #5 A = 16'h0043; B = 16'h5021; Cin = 0;
+    #5 A = 16'h0723; B = 16'h0429;
+    #5 A = 16'h0553; B = 16'h6420;
   
    #50 $finish;
 
