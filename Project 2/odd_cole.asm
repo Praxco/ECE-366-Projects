4516b700494cdaca60c9f849eb6a6783c17aa4d9
cgp2201 <carlosperez2201g@gmail.com>
2025-04-04 16:19:53 -0500

    Update odd_cole.asm

--- Project 2/odd_cole.asm
+++ Project 2/odd_cole.asm
@@ -11,14 +11,8 @@ main:
 	syscall
 	move $t4, $v0
 	
-	jal Odd
+	j Odd
 	
-	beq $a0, 1, print_true
-	beq $a0, 0, print_false
-	
-	#end program
-	li $v0, 10 #syscall code for exit
-	syscall
 	
 
 Odd:
@@ -29,20 +23,28 @@ Odd:
 repeat_subtract:
 	bge $t5, $t6, subtract #if m>=2 go to subtract
 	move $a0, $t5 # if m<2 the remainder is just the value of m (0 or 1)
-	jr $ra
+	j Print_conditions
 	
 subtract:
 	sub $t5, $t5, $t6 # m = m - 2
 	j repeat_subtract
 	
 print_true:
-	li $v0, 4           # syscall code for printing string
+    li $v0, 4           # syscall code for printing string
     la $a0, true_str    # Load address of "true" string
     syscall
-    jr $ra              # Return from function
+    j Exit              # Return from function
     
 print_false:
     li $v0, 4           # syscall code for printing string
     la $a0, false_str   # Load address of "false" string
     syscall
-    jr $ra              # Return from function
+    j Exit             # Return from function
+    
+Print_conditions:
+    beq $a0, 1, print_true
+    beq $a0, 0, print_false
+Exit:
+    	#end program
+    li $v0, 10 #syscall code for exit
+    syscall
